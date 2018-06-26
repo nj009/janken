83357d9e3b88b989debebee4e643b1b9c3b752b4
nj009 <darling.22ks@gmail.com>
2018-06-26 20:06:05 +0900

    修正

--- janken.py
+++ janken.py
@@ -1,31 +1,23 @@
 # -*- coding: utf-8 -*-
-word = input()
-print("your input: {}".format(word))
-
-#ランダムに、グー、チョキ、パー、を決める
-#こっちが、グーのとき、相手がグー、の時は、アイコ。チョキ、の時は、勝ち。パー、の時は、負け。
-#こっちが、パーのとき、相手がグー、の時は、勝ち。チョキの時は、負け。パー、の時は、アイコ。
-#こっちが、チョキの時、相手がグー、の時は、負け、チョキの時、アイコ、パーの時は、勝ち
-#あいこの場合、もう一回 改めて、最初に戻る
-#random, if, try
-
 import random
 
 dic = {"a": "グー", "b": "チョキ", "c": "パー"}
 
 print("じゃんけーん")
 print("a=グー, b=チョキ, c=パー,a,b,cのいずれかを入力")
+word = input()
+print("your input: {}".format(word))
 
 try:
-    user_choice: str = dic[user]
-    
-    choice_list = ["a", "b", "C"]
+    user_choice = dic.get(word)
+
+    choice_list = ["a", "b", "c"]
     pc = dic[random.choice(choice_list)]
-    
+
     draw = 'ドロー'
     win = '勝ち！'
     lose = '負け'
-    
+
     if user_choice == pc:
         judge = draw
 
@@ -37,20 +29,19 @@ try:
                 judge = lose
 
 
-elif user_choice == "チョキ":
-    if pc == "パー":
-        judge = win
+        elif user_choice == "チョキ":
+            if pc == "パー":
+                judge = win
             else:
                 judge = lose
 
-    else:
-        if pc == "グー":
-            judge = win
+        else:
+            if pc == "グー":
+                judge = win
             else:
                 judge = lose
-
-print("ありがとうございました。また！")
+    print(judge)
+    print("ありがとうございました。また！")
 
 except:
     print("a,b,cのいずれかを入力してください。")
-
